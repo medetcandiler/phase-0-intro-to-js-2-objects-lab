d917664317f04b6b9ab3c02334c5abe9c7b197b9
Medetcan Diler <medetdiler1996@gmail.com>
2023-05-06 22:43:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value ){
+  const updatedEmployee = {...employee};
+  updatedEmployee[key] = value ;
+  return updatedEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee , key , value ){
+  employee[key] = value;
+  return employee;
+}
+// console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'medet'))
+
+
+function deleteFromEmployeeByKey(employee, key){
+  const employeeClone = {...employee};
+  delete employeeClone[key];
+  return employeeClone
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee[key];
+  return employee
+}
